d4e754b6d3b7f97231c382295f279cc173b58b75
MayugaaSadayappan <mayugaa609@gmail.com>
2025-08-09 09:24:46 +0530

    Add files via upload

---
+++ P1-Basic Calculator/P2-Coding/Day4.js/day4.js
@@ -0,0 +1,6 @@
+//array
+let number=[1,2,"three",true];
+
+let names=new Array("jack","daniels");
+console.log(number[2]);
+console.log(names.length);
---
+++ P1-Basic Calculator/P2-Coding/String-Objects-Arrays/script.js
@@ -0,0 +1,5 @@
+
+let name1 = "Mayugaa";
+let age = 23;
+let sentence = `Hi! my name is ${name1} and my age is ${age}`;
+console.log(sentence);
